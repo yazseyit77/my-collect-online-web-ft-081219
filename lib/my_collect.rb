388de3f88a4a7c0c6c57348ff689b6a0745c9792
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 16:47:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,13 @@
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
 def my_collect(array) do |name|
   name.split(" ").first
 end
